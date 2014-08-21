912eb2401609288418e8ce000ba9e75e679a2f55
festaans <stephanus@glio.co.za>
2014-08-21 18:10:35 +0200

    Gulp + Angular + Bootstrap variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,32 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+var connect = require('gulp-connect');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app',
+    livereload: true
+  });
 });
+
+gulp.task('html', function () {
+  gulp.src('./app/*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('less', function () {
+  gulp.src('./app/assets/less/quickcast.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./app/assets/css'))
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./app/*.html'], ['html']);
+  gulp.watch(['./app/assets/less/*.less'], ['less']);
+});
+
+gulp.task('default', ['connect', 'watch']);
